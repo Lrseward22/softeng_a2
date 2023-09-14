4418fab23deea00efa25d2bf0ef45ba0ebb51725
jed-richards <jed22richards@gmail.com>
2023-09-14 14:50:55 -0500

    created skeleton of dairy and entry classes

---
+++ dairy.py
@@ -0,0 +1,37 @@
+from entry import Entry
+
+class Diary():
+    def __init__(self, **kwargs):
+        self.owner
+        self.num_entries = 0
+        pass
+
+    def write_entry(self, **kwargs):
+        # entry_name (check if unique)
+        # contents of entry (prompt for input)
+
+        """
+        create a new entry if name is unique
+        """
+
+        pass
+
+    def read_entry(self, **kwargs):
+        # entry_name to read
+
+        """
+        prtint an entry if it exists
+        """
+        pass
+
+    def delete_entry(self, **kwargs):
+        # entry_name to delete
+        
+        # ask if you really want to delete
+        """
+        delete an entry if it exists
+        """
+        pass
+
+    def list_entries(self):
+        pass
---
+++ entry.py
@@ -0,0 +1,17 @@
+class Entry():
+    def __init__(self):
+        self.entry_name
+        self._path
+        self.contents
+        pass
+
+    def create_file(self):
+        pass
+
+    # used by programmer
+    def __repr__(self):
+        pass 
+
+    # used by user
+    def __str__(self):
+        pass 
