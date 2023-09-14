cd8bd349c581cdff849e1d13b09652afd5e85d03
jed-richards <jed22richards@gmail.com>
2023-09-14 16:10:37 -0500

    basic usage created

--- src/__main__.py
+++
@@ -1,5 +0,0 @@
-from diary import Diary
-from entry import Entry
-
-if "__name__" == "__main__":
-    print("Hello from main")
--- src/entry.py
+++ src/entry.py
@@ -1,17 +1,14 @@
-class Entry():
+class Entry:
     def __init__(self):
         self.entry_name
         self._path
         self.contents
         pass
 
-    def create_file(self):
-        pass
-
     # used by programmer
     def __repr__(self):
-        pass 
+        pass
 
     # used by user
     def __str__(self):
-        pass 
+        pass
