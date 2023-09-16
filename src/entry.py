907b4f4a5d53b3c51e0e1d89fcfb8a74160532de
CETerry <randomcretin@hotmail.com>
2023-09-16 12:09:56 -0500

    fixxing merge conflict

--- src/entry.py
+++ src/entry.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-class Entry():
-    def __init__(self, entry_name: str):
-        self.entry_name = entry_name
-        self.entry_path
-        self.contents
-        pass
-
-    def create_file(self):
-        open(self.entry_path, "x")
-        pass
-
-=======
 class Entry:
     def __init__(self):
         self.entry_name
@@ -18,7 +5,6 @@ class Entry:
         self.contents
         pass
 
->>>>>>> cd8bd349c581cdff849e1d13b09652afd5e85d03
     # used by programmer
     def __repr__(self):
         pass
